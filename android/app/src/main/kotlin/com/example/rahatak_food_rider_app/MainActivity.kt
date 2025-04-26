3a457ee406b108660466b3e11bb11871618882f2
saiedfoyez23 <saiedfoyez@gmail.com>
2025-04-26 16:25:16 +0600

    add all code

---
+++ android/app/src/main/kotlin/com/example/rahatak_food_rider_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rahatak_food_rider_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get/get.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,DeviceOrientation.portraitDown]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Rahatak Food Rider App",
+      builder: (context,child) {
+        return Directionality(
+          textDirection: Get.locale.toString() == "en" ?
+          TextDirection.ltr :
+          TextDirection.rtl,
+          child: child!,
+        );
+      },
+      locale: Locale("en"),
+      localizationsDelegates: [
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: [
+        Locale("ar"),
+        Locale("en"),
+      ],
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
